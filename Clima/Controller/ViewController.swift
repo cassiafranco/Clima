208832834aa54eff4c4fa101ac05555600e7ad42
cassiafranco <cassia.developer@gmail.com>
2021-05-26 10:56:44 -0300

    created outlels

--- Clima/Controller/ViewController.swift
+++ Clima/Controller/ViewController.swift
@@ -8,13 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate{
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var conditionLabel: UIImageView!
+    
+    @IBOutlet weak var searchTextField: UITextField!
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        
+        searchTextField.delegate = self
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+        return true
+    }
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != ""{
+            return true
+            
+        }else {
+            textField.placeholder = "Type something"
+            return false
+        }
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        searchTextField.text = ""
+    }
 }
 
