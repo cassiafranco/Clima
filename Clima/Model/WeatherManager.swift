08dd2a4dc3c5e3e7cd58a01d59e5b8eb30524977
cassiafranco <cassia.developer@gmail.com>
2021-05-31 10:09:29 -0300

    updated api

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -9,10 +9,37 @@
 import Foundation
 
 struct WeatherManager {
-    let weatherURL = "http://api.openweathermap.org/data/2.5/weather?appid=87e7e2e8e1298b0539df5f886e37b50c&units=metric"
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=87e7e2e8e1298b0539df5f886e37b50c&units=metric"
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
-        print(urlString)
+        performRequest(urlString: urlString)
+    }
+    func performRequest (urlString: String){
+        
+        //1. Create URL
+        if let url = URL(string: urlString) {
+            
+            //2. Create URLSession
+            let session = URLSession(configuration: .default)
+            
+            //3. Give the session a task
+            let task = session.dataTask(with: url, completionHandler:handle(data:response:error:))
+        
+            //4. Start the task
+            task.resume()
+        }
+    }
+        
+        func handle(data: Data?, response: URLResponse?, error: Error?){
+            if error != nil{
+                print(error!)
+                return
+            }
+            if let safeData = data{
+                let dataString = String(data: safeData, encoding: .utf8)
+                print(dataString)
+                
+            }
     }
 }
