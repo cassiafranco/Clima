a74e29cab6c82c242f85be05ca7819c31822598d
cassiafranco <cassia.developer@gmail.com>
2021-06-22 12:12:05 -0300

    updated in xib

---
+++ Clima/Controller/ClimaViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ClimaViewController.swift
+//  Clima
+//
+//  Created by Cassia Franco on 21/06/21.
+//  Copyright © 2021 Cassia Franco. All rights reserved.
+//
+
+import UIKit
+
+class ClimaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
